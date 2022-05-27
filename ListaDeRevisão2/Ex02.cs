b0944cf529959fe02310f7626b76dd376932ce59
Lucas de Lima Nunes <93940387+monzadrifteiro@users.noreply.github.com>
2022-05-27 02:54:49 +0000

    Adicionei as resoluções da lista 5 e metade das resoluções da lista de revisão 2

---
+++ ListaDeRevisão2/Ex02.cs
@@ -0,0 +1,25 @@
+using System; 
+public class Program {
+  public static void Main(string[] args) {
+    
+    int n1 =int.Parse(Console.ReadLine());
+    int n2 =int.Parse(Console.ReadLine());
+    int n3 =int.Parse(Console.ReadLine());
+    int n4 =int.Parse(Console.ReadLine());
+
+    int notaf= (n1 + n2 + n3 + n4) / 4;
+    Console.WriteLine("Média = " + notaf);
+    
+    Console.WriteLine("Números menores que a média");
+    if (n1 < notaf) {Console.WriteLine(n1);}
+    if (n2 < notaf) {Console.WriteLine(n2);}
+    if (n3 < notaf) {Console.WriteLine(n3);}
+    if (n4 < notaf) {Console.WriteLine(n4);}
+
+    Console.WriteLine("Números maiores que a média");
+    if (n1 > notaf) {Console.WriteLine(n1);}
+    if (n2 > notaf) {Console.WriteLine(n2);}
+    if (n3 > notaf) {Console.WriteLine(n3);}
+    if (n4 > notaf) {Console.WriteLine(n4);}
+  }
+}
---
+++ ListaDeRevisão2/Ex03.cs
@@ -0,0 +1,28 @@
+using System; 
+public class Program {
+  public static void Main(string[] args) {
+
+    int a =int.Parse(Console.ReadLine());
+    int b =int.Parse(Console.ReadLine());
+    int c =int.Parse(Console.ReadLine());
+    int d =int.Parse(Console.ReadLine());
+
+    int numerosp = 0;
+    int numerosi = 0;
+
+    if (a % 2 == 0){numerosp = numerosp + a;}
+    else{numerosi = numerosi + a;}
+
+    if (b % 2 == 0){numerosp = numerosp + b;}
+    else{numerosi = numerosi + b;}
+
+    if (c % 2 == 0){numerosp = numerosp + c;}
+    else{numerosi = numerosi + c;}
+
+    if (d % 2 == 0){numerosp = numerosp + d;}
+    else{numerosi = numerosi + d;}
+
+    Console.WriteLine("Soma dos pares = " + numerosp);
+    Console.WriteLine("Soma dos ímpares = " + numerosi);
+  }
+}
